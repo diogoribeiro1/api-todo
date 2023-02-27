ce4c166680275a40560308547ac1463f60343a4f
diogoribeiro1 <diogoribeiro698@gmail.com>
2023-02-26 21:28:58 -0300

    add: others methods https

--- src/main/kotlin/com/devus/apikotlin/controllers/TodoController.kt
+++ src/main/kotlin/com/devus/apikotlin/controllers/TodoController.kt
@@ -3,12 +3,20 @@ package com.devus.apikotlin.controllers
 import com.devus.apikotlin.entities.Todo
 import com.devus.apikotlin.repositories.TodoRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Repository
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import java.util.Objects
+import java.util.Optional
 
 @RestController
 @RequestMapping("/todos")
@@ -25,5 +33,25 @@ class TodoController {
     @PostMapping
     fun create(@RequestBody todo: Todo): Todo{
         return repository.save(todo);
+    }
+
+    @GetMapping("/{id}")
+    fun getOne(@PathVariable id: Long): Optional<Todo>{
+        return repository.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    fun update(@PathVariable id: Long, @RequestBody todo: Todo): ResponseEntity<Todo>{
+        var todoPayload = repository.findById(id).get()
+        todoPayload.title = todo.title
+        todoPayload.description = todo.description
+        todoPayload = repository.save(todoPayload)
+        return ResponseEntity.status(HttpStatus.CREATED).body(todoPayload);
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable id: Long): ResponseEntity<Objects>{
+        var todoPayload = repository.deleteById(id)
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
